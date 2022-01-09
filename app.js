70f0ca0047a018b3b34912f898f96ed79b71c095
nedimbatilovic <nedimco2006@gmail.com>
2022-01-09 14:15:21 +0100

    update

--- app.js
+++ app.js
@@ -4,15 +4,43 @@ const searchInput = document.querySelector(".search-input");
 const submitButton = document.querySelector(".submit-btn");
 let searchValue;
 
+// event listenrs
+searchInput.addEventListener("input", updateInput);
+
+function updateInput(e) {}
+
 async function curatedPhotos() {
+    const dataFetch = await fetch("https://api.pexels.com/v1/curated", {
+        method: "GET",
+        headers: {
+            Accept: "application.json",
+            Authorization: auth,
+        },
+    });
+    const data = await dataFetch.json();
+    data.photos.forEach((photo) => {
+        console.log(photo);
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `
+            <img src=${photo.src.large}></img>
+            <p>${photo.photographer}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+async function searchPhotos(query) {
     const dataFetch = await fetch(
-        "https://api.pexels.com/v1/curated?per_page=15&page=1",
+        `https://api.pexels.com/v1/search?query=${query}`,
         {
             method: "GET",
             headers: {
-                Accept: "application/json",
+                Accept: "application.json",
                 Authorization: auth,
             },
         }
     );
 }
+
+curatedPhotos();
